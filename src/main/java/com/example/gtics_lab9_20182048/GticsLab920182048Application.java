7179ac6d73b924fc5853407f665aef7bd72f1cdd
ChristopherTerrones <106935547+ChristopherTerrones@users.noreply.github.com>
2024-11-10 07:19:21 -0500

    commit inicial

---
+++ src/main/java/com/example/gtics_lab9_20182048/GticsLab920182048Application.java
@@ -0,0 +1,13 @@
+package com.example.gtics_lab9_20182048;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsLab920182048Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GticsLab920182048Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gtics_lab9_20182048/GticsLab920182048ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gtics_lab9_20182048;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GticsLab920182048ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
